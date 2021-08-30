9a8b72d2993b5c5dea61daebebb0ca8959dd1b9d
jjcourtney <jcourtneywork@gmail.com>
2021-08-30 19:54:37 +0100

    moved prompt to own file

--- index.js
+++ index.js
@@ -1,57 +1,75 @@
-const inquirer = require("inquirer");
 
-const questions = require("./lib/questions/questions")
-const query = require("./query/query")
+const promptUser = require("./lib/prompt");
+
+const intital = require("./lib/questions/initial");
 
 
-const promptUser = questions => {
-    return inquirer.prompt(questions);
-  };
+
+
+
+// const handleInitResponse = async response => {
+//     switch (response){
+//         case "view all departments" : 
+//             query.logTable("department");
+//             break;
+//         case "view all roles": 
+//             query.logTable("role");
+//             break;
+//         case "view all employees":
+//             query.logTable("employee");
+//             break; 
+//         case "add department": {
+//                 const departmentToAdd = await promptUser(questions.departmentQuestions);
+//                 query.addDepartment(departmentToAdd);
+//             };
+//             break;
+//         case "add role": {
+//             const roleToAdd = await promptUser(questions.roleQuestions);
+//             query.addRole(roleToAdd);
+//         };
+//             break;
+//         case "add employee":{
+//             const employeeToAdd = await promptUser(questions.employeeQuestions);
+//             query.addEmployee(employeeToAdd);
+//         };
+//             break;
+//         case "update employee role": {
+//             let employeeToUpdate = await promptUser(questions.updateEmployeeQuestions);
+//             employeeToUpdate.table = "employee";
+//             employeeToUpdate.column = "role_id";
+            
+//             query.updateInfo(employeeToUpdate);
+//         }
+//         default:
+//             process.exit()
+        
+//     }
+
+//     init();
+// }
 
 const handleInitResponse = async response => {
     switch (response){
-        case "view all departments" : 
+        case "View" : 
             query.logTable("department");
             break;
-        case "view all roles": 
-            query.logTable("role");
+        case "Add" : 
+            query.logTable("department");
             break;
-        case "view all employees":
-            query.logTable("employee");
-            break; 
-        case "add department": {
-                const departmentToAdd = await promptUser(questions.departmentQuestions);
-                query.addDepartment(departmentToAdd);
-            };
+        case "Update" : 
+            query.logTable("department");
             break;
-        case "add role": {
-            const roleToAdd = await promptUser(questions.roleQuestions);
-            query.addRole(roleToAdd);
-        };
+        case "Delete" : 
+            query.logTable("department");
             break;
-        case "add employee":{
-            const employeeToAdd = await promptUser(questions.employeeQuestions);
-            query.addEmployee(employeeToAdd);
-        };
-            break;
-        case "update employee role": {
-            let employeeToUpdate = await promptUser(questions.updateEmployeeQuestions);
-            employeeToUpdate.table = "employee";
-            employeeToUpdate.column = "role_id";
-            
-            query.updateInfo(employeeToUpdate);
-        }
         default:
             process.exit()
-        
     }
-
-    init();
 }
 
 const init = () => {
     console.clear();
-    promptUser(questions.initialQuestion)
+    promptUser(intital.initial)
     .then((userInput) => {
         handleInitResponse(userInput.actionType)
         })
