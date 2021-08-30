be0f386c436c92df4f429cf9e2c9155b5ed3502f
jjcourtney <jcourtneywork@gmail.com>
2021-08-30 19:53:03 +0100

    viewEmployeesByDepartment

---
+++ query/view.js
@@ -0,0 +1,31 @@
+const query = require("./query");
+
+const view = table => query.logTable(table);
+
+const viewBy = (table, id) => {
+
+}
+const viewEmployeesByDepartment = departmentId => {
+    const sql = `SELECT employee.id AS Employee_ID, 
+    employee.first_name AS First_Name,
+    employee.last_name AS Last_Name,
+    employee.role_id As Role_ID,
+    employee.manager_id As Manager_ID,
+    role.title AS Role_title,
+    role.salary AS Salary, 
+    role.id As Role_ID, 
+    department.id AS Department_ID 
+    FROM employee 
+    LEFT JOIN (SELECT * FROM role
+    WHERE department_id = ${departmentId }) AS role
+    ON employee.role_id = role.id 
+    LEFT JOIN department 
+    ON role.department_id = department.id
+    WHERE role.department_id = ${departmentId }`
+                
+    query.doQuery(sql, "log")
+}
+
+module.exports = {
+    view,
+}
